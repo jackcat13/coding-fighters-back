f507d710651e6ae3d1b5dc5bbd8118600c6d6f60
Chris Henry <christophehenry11@gmail.com>
2024-06-07 16:07:09 +0200

    feat: implement game progress logic to make the game works in both back and front (#25)

---
+++ src/model/game_progress.rs
@@ -0,0 +1,22 @@
+use rocket::serde::{Deserialize, Serialize};
+
+///GameProgress entity to be stored in the database
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub struct GameProgress {
+    #[serde(rename = "_id")]
+    pub id: String,
+    pub current_question: i8,
+    pub question_number: i8,
+    pub question_content: Question,
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub struct Question {
+    pub question_text: String,
+    pub answer_1: String,
+    pub answer_2: String,
+    pub answer_3: String,
+    pub answer_4: String,
+    pub good_answer_number: i8,
+    pub topic: String,
+}
